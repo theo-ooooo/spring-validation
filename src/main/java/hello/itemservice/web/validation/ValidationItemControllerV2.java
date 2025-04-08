0b2912da8ab6af22c7bf506abb56770904b63c46
Theo <kkw.theo@gmail.com>
2025-04-08 18:01:10 +0900

    feat: Bean Validation

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -227,7 +227,11 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/{itemId}/edit")
-    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+    public String edit(@PathVariable Long itemId, @Validated @ModelAttribute Item item, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return "validation/v2/editForm";
+        }
         itemRepository.update(itemId, item);
         return "redirect:/validation/v2/items/{itemId}";
     }
